9f52b6657efe128fc36f6d5b45fb7ccbe6e621c5
Indira Avendano <indirajhenny@Indiras-MacBook-Pro.local>
2020-10-10 12:41:24 -0400

    integrated css/html into app.js

--- WiseUp/wise-up/src/App.js
+++ WiseUp/wise-up/src/App.js
@@ -4,7 +4,37 @@ function App() {
   return (
     // this will be majority of the HTML
     <div className="App">
-      Hello World! Hello again
+    <body>
+        <header>
+            <h1>WiseUp study buddie</h1>
+            <div>
+                <p>The app that helps you stay motivated</p>
+            </div>
+        </header>
+
+        <main>
+            <div>
+                <p>This is for the main content and directions for users</p>
+            </div>
+
+        </main>
+
+        <nav>
+            <div>
+                <p>Navigation</p>
+            </div>
+        </nav>
+
+        <aside>
+            <div>
+                <p>Changes and updates</p>
+            </div>
+        </aside>
+
+        <footer>
+            <p> Author: LYC</p>
+        </footer>
+    </body>
     </div>
   );
 }
