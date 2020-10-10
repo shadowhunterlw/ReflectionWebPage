2b102914dbd223161c3b8945666045301f9d93ca
Indira Avendano <indirajhenny@Indiras-MacBook-Pro.local>
2020-10-10 12:21:16 -0400

    Added React Web Page

---
+++ WiseUp/wise-up/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    // this will be majority of the HTML
+    <div className="App">
+      Hello World! Hello again
+    </div>
+  );
+}
+
+export default App;
